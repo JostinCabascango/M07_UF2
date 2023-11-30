757b835994a3351c9a957a1dc1abcb6d85749bba
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-11-30 17:45:41 +0100

    Refactor sign-in form

--- resources/views/auth/signIn.blade.php
+++ resources/views/auth/signIn.blade.php
@@ -9,19 +9,27 @@
 
 <body>
     <h1>{{$title}}</h1>
-    <form action="{{url('/jostin/signin')}}" method="post"></form>
-    @csrf
-    <!--campos del formulario para el inicio de sesión -->
-    <label for="username">Nombre de Usuario:</label>
-    <input type="text" id="username" name="username" required>
-
-    <label for="password">Contraseña:</label>
-    <input type="password" id="password" name="password" required>
-
-    <button type="submit">Iniciar Sesión</button>
+    <form action="{{url('login')}}" method="post" class="">
+        @csrf
+        <div class="">
+            <label class="" for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Introduce tu email">
+        </div>
+        <div class="">
+            <label class="" for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password"
+                placeholder="Introduce tu password">
+        </div>
+        <div class="">
+            <input type="checkbox" name="active" value="1" class="form-check-input" />
+            <label class="form-check-label">Remember me</label>
+        </div>
+        <div class="">
+            <button type="submit" class="">Submit</button>
+        </div>
     </form>
     <!-- Enlace para ir a SignUp -->
-    <p>¿No tienes una cuenta? <a href="{{ url('/marc/signup') }}">Regístrate</a></p>
+    <p>¿No tienes una cuenta? <a href="{{ url('/marc/signup') }}">Crea una cuenta</a></p>
 
 </body>
 
