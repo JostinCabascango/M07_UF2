0fdd5d9e05c4ecf561b60cc50e9926f1a49e6173
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-12-02 10:27:40 +0100

    Add store method to RegisterController

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     //
+    public function store(Request $request){
+        // Recoger los datos del formulario
+        $id = $request->input('user_id');
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        $password = $request->input('password');
+        $email = $request->input('email');
+        $role = $request->input('role');
+        $active = $request->input('active');
+    }
 }
