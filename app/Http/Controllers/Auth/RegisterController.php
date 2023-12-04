1e93aaf20c51b757668cdc4e4390ceb6888e1b34
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-12-04 20:53:42 +0100

    Refactor store method in RegisterController to handle different user roles

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     //
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         // Recoger los datos del formulario
         $id = $request->input('user_id');
         $name = $request->input('name');
@@ -17,5 +18,29 @@ class RegisterController extends Controller
         $email = $request->input('email');
         $role = $request->input('role');
         $active = $request->input('active');
+        // Si el rol es "alumnat", es un alumno y se le muestra la vista de alumno
+        if ($role == "alumnat") {
+            $alumno = [
+                'user_id' => $id,
+                'name' => $name,
+                'surname' => $surname,
+                'email' => $email,
+                'role' => $role,
+                'active' => $active
+            ];
+            return view('users.alumne')->with('alumno', $alumno);
+        }
+        // Si el rol es "professorat", es un profesor y se le muestra la vista de profesor
+        if ($role == "professorat") {
+            $profesor = [
+                'user_id' => $id,
+                'name' => $name,
+                'surname' => $surname,
+                'email' => $email,
+                'role' => $role,
+                'active' => $active
+            ];
+            return view('users.professor')->with('profesor', $profesor);
+        }
     }
 }
