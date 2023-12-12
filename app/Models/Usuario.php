19496085169976faa1f7ff1a10a62573175f4d8e
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-12-12 19:50:38 +0100

    Update Usuario model with table name, fillable fields, hidden fields, and casts

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     use HasFactory;
+    // Nombre de la tabla en la base de datos
+    protected $table = 'usuarios';
+    // Campos que se pueden rellenar en la base de datos
+    protected $fillable = [
+        'name',
+        'surname',
+        'password',
+        'email',
+        'role',
+        'active'
+    ];
+    protected $hidden = [
+        'password', // Oculta el campo 'password' en las consultas
+    ];
+    protected $casts = [];
 }
