7aa625e2ae42763825eec148e3d77548b2acd671
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-12-20 18:01:46 +0100

    Update user views with welcome message and user information

--- resources/views/users/professor.blade.php
+++ resources/views/users/professor.blade.php
@@ -10,12 +10,24 @@
 </head>
 
 <body class="bg-gray-100">
-    <!--Mensaje de bienvenida al usuario -->
-    <h1 class="text-4xl font-bold text-center text-blue-600 py-4">Bienvenido professor. Tu correo és {{ $email }}.</h1>
-    <!-- Enlace para ir al home -->
-    <div class="flex justify-center">
-        <a href="{{ route('home') }}"
-            class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Home</a>
+    <div class="container mx-auto px-4">
+        <!--Mensaje de bienvenida al profesor -->
+        <h1 class="text-4xl font-bold text-center text-blue-600 py-4">Bienvenido, {{ session('usuario')->name }}.</h1>
+        <!-- Enlace para ir al home -->
+        <div class="flex justify-center">
+            <a href="{{ route('home') }}"
+                class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Home</a>
+        </div>
+        <!-- Información del profesor -->
+        <div class="mt-6">
+            <div class="bg-white shadow-md rounded p-6">
+                <h1 class="block text-gray-700 font-bold mb-2 text-xl text-center py-4 bg-gray-200 rounded-t">
+                    Información del Profesor</h1>
+                <p><strong>Nombre:</strong> {{ session('usuario')->name }}</p>
+                <p><strong>Email:</strong> {{ session('usuario')->email }}</p>
+                <p><strong>Rol:</strong> {{ session('usuario')->role }}</p>
+            </div>
+        </div>
     </div>
 </body>
 
