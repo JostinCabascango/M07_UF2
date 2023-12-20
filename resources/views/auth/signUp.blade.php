6e564e7314e17843a46ab7820ffa3c0a88241bc3
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-12-20 18:06:33 +0100

    Add error messages for invalid input fields in signIn and signUp views

--- resources/views/auth/signUp.blade.php
+++ resources/views/auth/signUp.blade.php
@@ -15,18 +15,35 @@
         <div class="mb-4">
             <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nombre</label>
             <input type="text" name="name" class="form-input w-full" />
+            <!-- Mostrar un mensaje de error si el nombre es incorrecto -->
+            @error('name')
+                <small class="text-red-500">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-4">
             <label for="surname" class="block text-gray-700 text-sm font-bold mb-2">Apellido</label>
             <input type="text" name="surname" class="form-input w-full" />
+            <!-- Mostrar un mensaje de error si el apellido es incorrecto -->
+            @error('surname')
+                <small class="text-red-500">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-4">
             <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Contraseña</label>
             <input type="password" name="password" class="form-input w-full" />
+            <!-- Mostrar un mensaje de error si el password es incorrecto -->
+            @error('password')
+                <small class="text-red-500">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-4">
             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
             <input type="email" name="email" class="form-input w-full" />
+            <!-- Mostrar un mensaje de error si el email es incorrecto -->
+            @error('email')
+                <small class="text-red-500">{{ $message }}</small>
+            @enderror
+
         </div>
         <div class="mb-4">
             <label for="role" class="block text-gray-700 text-sm font-bold mb-2">Rol</label>
@@ -34,18 +51,26 @@
                 <option value="estudiante">Alumno</option>
                 <option value="profesor">Profesor</option>
                 <option value="centro">Admin</option>
-
             </select>
+            <!-- Mostrar un mensaje de error si el rol es incorrecto -->
+            @error('role')
+                <small class="text-red-500">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-4">
             <label for="active" class="block text-gray-700 text-sm font-bold mb-2">Activo</label>
+            <input type="hidden" name="active" value="0" />
             <input type="checkbox" name="active" value="1" class="form-checkbox" />
+            <!-- Mostrar un mensaje de error si el active es incorrecto -->
+            @error('active')
+                <small class="text-red-500">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-4">
             <input type="submit" value="Enviar" class="bg-blue-500 text-white py-2 px-4 rounded" />
         </div>
     </form>
-    <p class="text-center">¿tienes una cuenta? <a href="{{ route('login.create') }}" class="text-blue-500">Inicia
+    <p class="text-center mb-5">¿Tienes una cuenta? <a href="{{ route('login.create') }}" class="text-blue-500">Inicia
             sesion</a></p>
 </body>
 
