d56cd313387751875c3354a62d3d973576167179
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-12-20 18:01:01 +0100

    Refactor admin/centre.blade.php to improve user interface

--- resources/views/admin/centre.blade.php
+++ resources/views/admin/centre.blade.php
@@ -10,32 +10,46 @@
 </head>
 
 <body class="bg-gray-100">
-    <!--Mensaje de bienvenida al usuario -->
-    <h1 class="text-4xl font-bold text-center text-blue-600 py-4">Bienvenido administrador. Tu correo és {{ $email }}.
-    </h1>
-    <!-- Enlace para ir al home -->
-    <div class="flex justify-center">
-        <a href="{{ route('home') }}"
-            class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Home</a>
+    <div class="container mx-auto px-4">
+        <!--Mensaje de bienvenida al administrador -->
+        <h1 class="text-4xl font-bold text-center text-blue-600 py-4">Bienvenido administrador.</h1>
+        <!-- Enlace para ir al home -->
+        <div class="flex justify-center">
+            <a href="{{ route('home') }}"
+                class="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Home</a>
+        </div>
+        <!-- Listado de usuarios -->
+        <div class="mt-6">
+            <div class="bg-white shadow-md rounded overflow-x-auto">
+                <h1 class="block text-gray-700 font-bold mb-2 text-xl text-center py-4 bg-gray-200 rounded-t">Usuarios
+                </h1>
+                <table class="w-full">
+                    <thead>
+                        <tr class="bg-gray-100">
+                            <th class="px-4 py-2">ID</th>
+                            <th class="px-4 py-2">Nombre</th>
+                            <th class="px-4 py-2">Apellido</th>
+                            <th class="px-4 py-2">Email</th>
+                            <th class="px-4 py-2">Rol</th>
+                            <th class="px-4 py-2">Activo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($usuarios as $usuario)
+                        <tr class="bg-white hover:bg-gray-100 transition-colors duration-200">
+                            <td class="border px-4 py-2">{{ $usuario->id }}</td>
+                            <td class="border px-4 py-2">{{ $usuario->name }}</td>
+                            <td class="border px-4 py-2">{{ $usuario->surname }}</td>
+                            <td class="border px-4 py-2">{{ $usuario->email }}</td>
+                            <td class="border px-4 py-2">{{ $usuario->role }}</td>
+                            <td class="border px-4 py-2">{{ $usuario->active }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-    <!-- Lista de profesores-->
-    <h2 class="text-3xl font-semibold text-center text-gray-800 my-8">Lista de Profesores</h2>
-    <table class="w-full max-w-4xl mx-auto border-collapse border border-gray-300">
-        <tr class="bg-blue-600 text-white">
-            <th class="p-2 border border-gray-300">Id</th>
-            <th class="p-2 border border-gray-300">Nombre</th>
-            <th class="p-2 border border-gray-300">Email</th>
-            <th class="p-2 border border-gray-300">Curso</th>
-        </tr>
-        @foreach ($profesores as $profesor)
-        <tr class="bg-white hover:bg-gray-200">
-            <td class="p-2 border border-gray-300">{{ $profesor['id'] }}</td>
-            <td class="p-2 border border-gray-300">{{ $profesor['name']}}</td>
-            <td class="p-2 border border-gray-300">{{ $profesor['email'] }}</td>
-            <td class="p-2 border border-gray-300">{{ $profesor['course'] }}</td>
-        </tr>
-        @endforeach
-    </table>
 </body>
 
 </html>
