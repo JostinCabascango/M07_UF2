43d155b2ab541fa64ce5703f471cbc45604526d3
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2024-01-11 18:40:26 +0100

    Add CRUD operations for managing teachers in AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,6 +14,34 @@ class AdminController extends Controller
         $teachers = Usuario::where('role', 'profesor')->get();
         return view('admin.centre')->with('teachers', $teachers);
     }
-
-
+    // Ruta para la vista de un administrador para crear un profesor
+    public function create()
+    {
+        return view('admin.create');
+    }
+    // Almacenar un profesor en la base de datos
+    public function store(Request $request)
+    {
+        return view('admin.store');
+    }
+    // Ruta para mostrar un profesor en concreto
+    public function show($id)
+    {
+        return view('admin.show');
+    }
+    // Ruta que muestra el formulario para editar un profesor en concreto
+    public function edit($id)
+    {
+        return view('admin.edit');
+    }
+    // Actualizara un profesor existente en la base de datos
+    public function update(Request $request, $id)
+    {
+        return view('admin.update');
+    }
+    // Eliminara un profesor existente en la base de datos
+    public function destroy($id)
+    {
+        return view('admin.destroy');
+    }
 }
