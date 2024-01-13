22fb56c530c11f9e649d9578bfaf68214efd5d5a
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2024-01-13 11:43:19 +0100

    Refactor routes to use route prefixes

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // Ruta para la vista de inicio
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::view('/', 'welcome')->name('home');
 
-// Ruta de la vista de inicio de sesion de un usuario
-Route::get('/login', [SignController::class, 'showSignIn'])->name('login.create');
 
-// Ruta de la vista de registro de un usuario
+Route::prefix('login')->group(function () {
+    Route::get('/', [SignController::class, 'showSignIn'])->name('login.create');
+    Route::post('/', [LoginController::class, 'store'])->name('login.store');
+});
+
 Route::get('/signup', [SignController::class, 'showSignUp'])->name('signup.create');
-
-// Ruta para validar el inicio de sesion de un usuario
-Route::post('/login', [LoginController::class, 'store'])->name('login.store');
-
-// Ruta para registrar un usuario nuevo
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
 // Ruta para la vista de un alumno
@@ -39,35 +34,23 @@ Route::get('/alumno', function () {
     return view('users.alumne');
 })->name('alumno.index');
 
-/* Rutas de un administrador */
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/create', [AdminController::class, 'create'])->name('admin.create');
+    Route::post('/', [AdminController::class, 'store'])->name('admin.store');
+    Route::get('/{id}', [AdminController::class, 'show'])->name('admin.show');
+    Route::get('/{id}/edit', [AdminController::class, 'edit'])->name('admin.edit');
+    Route::put('/{id}', [AdminController::class, 'update'])->name('admin.update');
+    Route::delete('/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
+});
 
-// Ruta para la vista de un administrador
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-// Ruta para la vista de un administrador para crear un profesor
-Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
-// Ruta para la vista de un administrador para crear un profesor
-Route::post('/admin', [AdminController::class, 'store'])->name('admin.store');
-// Ruta para mostrar un profesor en concreto
-Route::get('/admin/{id}', [AdminController::class, 'show'])->name('admin.show');
-// Ruta para la vista de un administrador para editar un profesor
-Route::get('/admin/{id}/edit', [AdminController::class, 'edit'])->name('admin.edit');
-// Ruta para la vista de un administrador para editar un profesor
-Route::put('/admin/{id}', [AdminController::class, 'update'])->name('admin.update');
-// Ruta para la vista de un administrador para eliminar un profesor
-Route::delete('/admin/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
-
-/* Rutas de un profesor */
-//
-Route::get('/profesor', [TeacherController::class, 'index'])->name('teacher.index');
-//
-Route::get('/profesor/create', TeacherController::class . 'create')->name('teacher.create');
-//
-Route::post('/profesor', TeacherController::class . 'store')->name('teacher.store');
-//
-Route::get('/profesor/{id}', TeacherController::class . 'show')->name('teacher.show');
-//
-Route::get('/profesor/{id}/edit', TeacherController::class . 'edit')->name('teacher.edit');
-//
-Route::put('/profesor/{id}', TeacherController::class . 'update')->name('teacher.update');
-//
-Route::delete('/profesor/{id}', TeacherController::class . 'destroy')->name('teacher.destroy');
+Route::prefix('profesor')->group(function () {
+    Route::get('/', [TeacherController::class, 'index'])->name('teacher.index');
+    Route::get('/create', [TeacherController::class, 'create'])->name('teacher.create');
+    Route::post('/', [TeacherController::class, 'store'])->name('teacher.store');
+    Route::get('/{id}', [TeacherController::class, 'show'])->name('teacher.show');
+    Route::get('/{id}/edit', [TeacherController::class, 'edit'])->name('teacher.edit');
+    Route::put('/{id}', [TeacherController::class, 'update'])->name('teacher.update');
+    Route::delete('/{id}', [TeacherController::class, 'destroy'])->name('teacher.destroy');
+});
+Route::view('/alumno', 'users.alumne')->name('alumno.index');
