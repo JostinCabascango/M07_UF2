1637276226bbd41c145c2eaa3c0457fcb5736c96
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2024-01-13 13:57:04 +0100

    Add FileController and update File model

---
+++ app/Http/Controllers/Files/FileController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Files;
+
+use App\Http\Controllers\Controller;
+use App\Models\File;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $files = File::all();
+        return view('files.index', compact('files'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(File $file)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(File $file)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, File $file)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(File $file)
+    {
+        //
+    }
+}
